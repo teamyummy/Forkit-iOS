d24387090f5f2b7ce7f643e120e7d0be908cb7ce
yhkim0426 <replaysite9@gmail.com>
2016-11-28 22:21:38 +0900

    Add custom UI components, Add Network Privacy at Info.plist

---
+++ Forkit/Forkit/CustomTitleLabel.m
@@ -0,0 +1,35 @@
+//
+//  CustomTitleLabel.m
+//  Forkit
+//
+//  Created by Nexus_MYT on 2016. 11. 28..
+//  Copyright © 2016년 david. All rights reserved.
+//
+
+#import "CustomTitleLabel.h"
+
+@implementation CustomTitleLabel
+
+- (instancetype)initWithTitle:(NSString *)title
+                        frame:(CGRect)frame
+                    textColor:(UIColor *)textColor
+{
+    self = [super init];
+    if (self) {
+        self.font = [UIFont boldSystemFontOfSize:30];
+        self.textColor = textColor;
+        self.text = title;
+        self.textAlignment = NSTextAlignmentCenter;
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
